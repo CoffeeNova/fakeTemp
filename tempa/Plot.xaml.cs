14a2b74a1f98fba0bbe5fd2b45197583f6c22007
CoffeeJelly <dnm.nova@gmail.com>
2016-10-10 23:15:46 +0300

    10.10.16(3)

--- tempa/Plot.xaml.cs
+++ tempa/Plot.xaml.cs
@@ -11,6 +11,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+using System.Threading;
+using System.ComponentModel;
+
 namespace tempa
 {
     /// <summary>
@@ -18,9 +40,18 @@ namespace tempa
     /// </summary>
     public partial class Plot : Window
     {
-        public Plot()
+        public Plot(List<Termometer> data)
         {
             InitializeComponent();
+            TermoData = data;
+            FirstDate = TermoData.First().MeasurementDate;
+            LastDate = TermoData.Last().MeasurementDate;
         }
+
+        public DateTime FirstDate { get; private set; }
+
+        public DateTime LastDate { get; private set; }
+
+        public List<Termometer> TermoData { get; set; }
     }
 }
--- tempa/Properties/VersionInfo.cs
+++ tempa/Properties/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.14.110")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.14.113")]
 
 
